bd34587ee24c0c32edda289326a3cc6c09701250
Zachary Weeden <zweeden@datto.com>
2017-11-30 17:46:25 -0500

    So this is a thing -_-

---
+++ adicart.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3.6
+import os
+import random
+import requests
+from selenium import webdriver
+from requests.utils import dict_from_cookiejar
+
+class WebSession:
+	def __init__(self):
+		# add user agent array and randomly select for header
+		self.session = requests.session()
+		self.headers = {'User-Agent':self.get_random_user_agent()}
+		self.driver = None
+
+	def get_random_user_agent(self):
+		user_agent_strings = [
+			'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4',
+			'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4',
+			'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
+			'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5',
+			'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5',
+			'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
+			'Mozilla/5.0 (iPad; CPU OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8L1 Safari/6533.18.5',
+			'Mozilla/5.0 (Linux; U; en-us; KFAPWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.13 Safari/535.19 Silk-Accelerated=true',
+			'Mozilla/5.0 (Linux; U; en-us; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.13 Safari/535.19 Silk-Accelerated=true',
+			'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.141.16-Gen4_11004310) AppleWebkit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true',
+			'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+			'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36',
+			'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19',
+			'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+',
+			'Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36',
+			'Mozilla/5.0 (Linux; U; Android 2.3; en-us; SAMSUNG-SGH-I717 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+			'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+			'Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+			'Mozilla/5.0 (Linux; Android 4.2.2; GT-I9505 Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.59 Mobile Safari/537.36',
+			'Mozilla/5.0 (Linux; U; Android 2.2; en-us; SCH-I800 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+		]
+		return random.choice(user_agent_strings)
+
+	def _request(self, method, url, params=None, data=None, headers=None):
+		# add passed headers
+		if headers is not None:
+			for key in headers.keys():
+				self.headers[key] = headers[key]
+		# implement method params
+		self.response = self.session.request(method, url, headers=self.headers)
+
+	def fetch_cookies(self):
+		return dict_from_cookiejar(self.response.cookies)
+
+	def start_driver(self):
+		self.driver = webdriver.Chrome(f'{os.getcwd()}/chromedriver') # chromedriver bin must be in folder of invocation
+
+	def transfer_cookies(self, cookie_transfer_url):
+		if self.driver is None:
+			self.start_driver()
+		self.driver.get(cookie_transfer_url) # commonly carts
+		self.driver.delete_all_cookies()
+		for key, value in self.fetch_cookies().items():
+		    self.driver.add_cookie({'name': key, 'value': value})
+		self.driver.refresh()
+
+# example
+style_code = 'BZ0223'
+size_code = '670'
+cart = WebSession() # creates a session with headers
+cart._request('GET', f'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-UpdateItems?qty_0=1&pid_0=%00{style_code}_{size_code}%00')
+cart.transfer_cookies('https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show')
+
